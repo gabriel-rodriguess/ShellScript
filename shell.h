6a2ce324570d87f4b8be53e93c961047a163a295
Gabriel Gustavo <rodriguesgg36@gmail.com>
2020-08-27 21:18:26 -0300

    separacao dos arquivos

---
+++ shell.h
@@ -0,0 +1,24 @@
+#ifndef _SHELL
+#define _SHELL
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "shell.c"
+#endif
+
+char ** LerComandos();
+int PegaQuantidadeDeComandos(char ** comandos);
+int * PegaSequenciaDePipes(int inicio, int argc, char ** argv);
+int EhOperador(char * palavra);
+int ContaPipe(int inicio, int argc, char ** argv);
+char ** RemoveOperadores(int argc, char ** argv);
+int EncontraOperador(char * operador, int argc, char ** argv);
+int * DefineEntradaDoPipe(int argc, char ** argv);
+int CopiaArquivo(int entradaDescritorDeArquivo, int saidaDescritorDeArquivo);
+int * ExecutaSequenciaDeComandos(char ** comandos, int quantidadeDeComandos, int * inicioDoComando, int * entradaDoPipe, int * saidaDoPipe);
+int * DefineSaidaDoPipe(int argc, char ** argv);
+
